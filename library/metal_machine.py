20c65a3dd3b23160c33b062a2947982540f37b03
Gerrit <tirreg91@googlemail.com>
2020-09-16 15:46:24 +0200

    Add metal_machine module.

---
+++ library/metal_machine.py
@@ -0,0 +1,230 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+try:
+    from metal_python.api import MachineApi
+    from metal_python import models
+
+    METAL_PYTHON_AVAILABLE = True
+except ImportError:
+    METAL_PYTHON_AVAILABLE = False
+
+from ansible.module_utils.basic import AnsibleModule
+from ansible.module_utils.metal import AUTH_SPEC, init_driver_for_module
+
+ANSIBLE_METADATA = {
+    'metadata_version': '0.1',
+    'status': ['preview'],
+    'supported_by': 'community'
+}
+
+DOCUMENTATION = '''
+---
+module: metal_machine
+
+short_description: A module to manage metal machine entities
+
+version_added: "2.8"
+
+description:
+    - Manages machine entities in the metal-api.
+    - Requires metal_python to be installed.
+    - Cannot update entities.
+
+options:
+    id:
+        description:
+            - The id of the machine, user for specific machine allocation.
+        required: false
+    name:
+        description:
+            - The name of the machine.
+        required: false
+    description:
+        description:
+            - The description of the machine.
+        required: false
+    hostname:
+        description:
+            - The hostname of the machine.
+        required: true
+    project:
+        description:
+            - The project of the machine.
+        required: true
+    partition:
+        description:
+            - The partition of the machine.
+        required: false
+    image:
+        description:
+            - The image of the machine.
+        required: true
+    size:
+        description:
+            - The size of the machine.
+        required: false    
+    networks:
+        description:
+            - The networks of the machine.
+        required: true    
+    tags:
+        description:
+            - The tags of the machine.
+        required: false              
+    state:
+        description:
+          - Assert the state of the machine.
+          - >-
+            Use C(present) to create or update a machine and C(absent) to
+            release it.
+        default: present
+        choices:
+          - absent
+          - present
+
+author:
+    - metal-stack
+'''
+
+EXAMPLES = '''
+- name: allocate a machine
+  metal_machine:
+    name: my-machine
+    description: "my machine"
+    hostname: my-machine
+    networks: 
+    - internet
+    - 5d30b3af-cb2a-4aa3-84e8-52dbf94a326b
+    size: c1-xlarge-x86
+    partition: fra-equ01
+    project: 9ec6882a-cd94-42a7-b667-ffaed43557c7
+
+- name: release a machine
+  metal_machine:
+    id: 306bc4ad-33cd-4744-8c6a-6b601f7179ea
+    state: absent
+'''
+
+RETURN = '''
+id:
+  description:
+    - machine id
+  returned: always
+  type: str
+  sample: 306bc4ad-33cd-4744-8c6a-6b601f7179ea
+'''
+
+
+class Instance(object):
+    def __init__(self, module):
+        if not METAL_PYTHON_AVAILABLE:
+            raise RuntimeError("metal_python must be installed")
+
+        self._module = module
+        self.changed = False
+        self._machine = dict()
+        self.id = module.params.get('id')
+        self._name = module.params.get('name')
+        self._description = module.params.get('description')
+        self._hostname = module.params.get('hostname')
+        self._project = module.params.get('project')
+        self._partition = module.params.get('partition')
+        self._image = module.params.get('image')
+        self._size = module.params.get('size')
+        self._networks = module.params.get('networks')
+        self._tags = module.params.get('tags')
+        self._ssh_pub_keys = module.params.get('ssh_pub_keys')
+        self._userdata = module.params.get('userdata')
+        self._state = module.params.get('state')
+        self._driver = init_driver_for_module(self._module)
+        self._machine_client = MachineApi(api_client=self._driver.client)
+
+    def run(self):
+        if self._module.check_mode:
+            return
+
+        self._find()
+
+        if self._state == "present":
+            if self._machine:
+                return
+
+            self._machine_allocate()
+            self.changed = True
+
+        elif self._state == "absent":
+            if not self.id:
+                self._module.fail_json(msg="id is a required argument when state is absent")
+            if self._machine:
+                self._machine_free()
+                self.changed = True
+
+    def _find(self):
+        if not self.id:
+            return
+
+        self._machine = self._machine_client.find_machine(self.id)
+
+    def _machine_allocate(self):
+        networks = list()
+        for n in self._networks:
+            networks.append(models.V1MachineAllocationNetwork(autoacquire=True, networkid=n))
+
+        r = models.V1MachineAllocateRequest(
+            uuid=self.id if self.id else None,
+            name=self._name,
+            description=self._description,
+            hostname=self._hostname,
+            partitionid=self._partition,
+            projectid=self._project,
+            imageid=self._image,
+            sizeid=self._size,
+            networks=networks,
+            tags=self._tags,
+            ssh_pub_keys=self._ssh_pub_keys,
+            user_data=self._userdata,
+        )
+
+        self._machine = self._machine_client.allocate_machine(r)
+        self.id = self._machine.id
+
+    def _machine_free(self):
+        self._machine_client.free_machine(self.id)
+
+
+def main():
+    argument_spec = AUTH_SPEC.copy()
+    argument_spec.update(dict(
+        id=dict(type='str', required=False),
+        name=dict(type='str', required=False),
+        description=dict(type='str', required=False),
+        hostname=dict(type='str', required=True),
+        project=dict(type='str', required=True),
+        partition=dict(type='str', required=False),
+        image=dict(type='str', required=False),
+        size=dict(type='str', required=False),
+        networks=dict(type='list', required=True),
+        tags=dict(type='list', default=list(), required=False),
+        ssh_pub_keys=dict(type='list', default=list(), required=False),
+        userdata=dict(type='str', required=False),
+        state=dict(type='str', choices=['present', 'absent'], default='present'),
+    ))
+    module = AnsibleModule(
+        argument_spec=argument_spec,
+        supports_check_mode=True,
+    )
+
+    instance = Instance(module)
+
+    instance.run()
+
+    result = dict(
+        changed=instance.changed,
+        ip=instance.id,
+    )
+
+    module.exit_json(**result)
+
+
+if __name__ == '__main__':
+    main()
